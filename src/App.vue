bbf3db652ac7cbfe3c99d73c9f6dc850b0484534
Annlix <Annlix@outlook.com>
2019-12-30 07:53:42 +0800

    no message

--- src/App.vue
+++ src/App.vue
@@ -14,119 +14,162 @@ export default {
     return {
       forms: [
         {
-          guid: this.guid.gen(),
-          label: { name: '颜色选择器' },
+          label: { 
+            name: '颜色选择器' 
+          },
           type: 'color',
           placeholder: '请选择颜色',
         },
         {
-          guid: this.guid.gen(),
-          label: { name: '日期选择器' },
+          label: { 
+            name: '日期选择器' 
+          },
           type: 'date',
           placeholder: '请选择日期'
         },
         {
-          guid: this.guid.gen(),
-          label: { name: '日期时间选择器' },
+          label: { 
+            name: '日期时间选择器' 
+          },
           type: 'datetime',
           placeholder: '请选择日期时间'
         },
         {
-          guid: this.guid.gen(),
-          label: { name: '日期时间选择器本地' },
+          label: { 
+            name: '日期时间选择器本地' 
+          },
           type: 'datetime-local',
           placeholder:'请选择日期时间'
         },
         {
-          guid: this.guid.gen(),
-          label: { name: '邮箱' },
+          label: { 
+            name: '邮箱' 
+          },
           type: 'email',
           placeholder: '请输入邮箱'
         },
         {
-          guid: this.guid.gen(),
-          label: { name: '文件' },
+          label: { 
+            name: '文件' 
+          },
           type: 'file',
           placeholder: '请选择文件'
         },
         {
-          guid: this.guid.gen(),
-          label: { name: '隐藏域' },
+          label: { 
+            name: '隐藏域' 
+          },
           type: 'hidden',
           placeholder: '这是隐藏域'
         },
         {
-          guid: this.guid.gen(),
-          label: { name: '图片' },
+          label: { 
+            name: '图片' 
+          },
           type: 'image',
           placeholder: '请选择图片',
           mime: 'image/*'
         },
         {
-          guid: this.guid.gen(),
-          label: { name: '月份选择器' },
+          label: { 
+            name: '月份选择器' 
+          },
           type: 'month',
           placeholder: '请选择月份'
         },
         {
-          guid: this.guid.gen(),
-          label: { name: '密码' },
+          label: { 
+            name: '密码' 
+          },
           type: 'password',
           placeholder: '请输入密码'
         },
         {
-          guid: this.guid.gen(),
-          label: { name: '数字输入框' },
+          label: { 
+            name: '数字输入框' 
+          },
           type: 'number',
           placeholder: '请输入数字'
         },
         {
-          guid: this.guid.gen(),
-          label: { name: '单选' },
+          label: { 
+            name: '单选' 
+          },
           type: 'radio',
-          placeholder: '请选择'
+          list: {
+            defaltValue: 'msft',
+            name: 'co',
+            options: [
+              { title: 'Microsoft', value: 'msft' },
+              { title: 'Google', value: 'gogl' },
+              { title: 'Amazon', value: 'amaz' },
+              { title: 'Facebook', value: 'facb' }
+            ]
+          }
         },
         {
-          guid: this.guid.gen(),
-          label: { name: '滑块' },
+          label: {
+            name: '多选'
+          },
+          type: 'checkbox',
+          list: {
+            defaltValue: ['apple', 'orange'],
+            options: [
+              { title: 'Apple', value: 'apple' },
+              { title: 'Orange', value: 'orange' },
+              { title: 'Banne', value: 'banne' },
+              { title: 'CocaCola', value: 'coca' }
+            ]
+          }
+        },
+        {
+          label: { 
+            name: '滑块' 
+          },
           type: 'range',
           placeholder: '这是滑块'
         },
         {
-          guid: this.guid.gen(),
-          label: { name: '搜索' },
+          label: { 
+            name: '搜索' 
+          },
           type: 'searchs',
           placeholder: '请输入关键词',
           readonly: false
         },
         {
-          guid: this.guid.gen(),
-          label: { name: '电话' },
+          label: { 
+            name: '电话' 
+          },
           type: 'tel',
           placeholder: '请输入电话号码'
         },
         {
-          guid: this.guid.gen(),
-          label: { name: '文本框' },
+          label: { 
+            name: '文本框' 
+          },
           type: 'text',
           placeholder: '请输入文本',
           readonly: true,
         },
         {
-          guid: this.guid.gen(),
-          label: { name: '时间' },
+          label: { 
+            name: '时间' 
+          },
           type: 'time',
           placeholder: '请选择时间'
         },
         {
-          guid: this.guid.gen(),
-          label: { name: '网址' },
+          label: { 
+            name: '网址' 
+          },
           type: 'url',
           placeholder: '请输入网址'
         },
         {
-          guid: this.guid.gen(),
-          label: { name: '周' },
+          label: { 
+            name: '周' 
+          },
           type: 'week',
           placeholder: '请选择周'
         }
--- src/components/AdForm.vue
+++ src/components/AdForm.vue
@@ -2,12 +2,12 @@
   <div>
     <form>
       <div v-for="item of forms" :key="item.guid" v-show="item.type !== 'hidden'">
-        <label>
+        <label v-if="['radio'].indexOf(item.type) == -1">
           {{item.label.name}} 
           <input 
             :type="item.type ? item.type : 'text'" 
             :placeholder="item.placeholder" 
-            v-if="item.type !== 'file' && !item.readonly" 
+            v-if="['file'].indexOf(item.type) === -1&& !item.readonly" 
             :autofocus='item.autofocus' 
             :disabled='item.disabled'
             :value="item.value"
@@ -34,19 +34,46 @@
             :autocomplete="item.autocomplete"
             readonly
           />
+          <!-- 文件上传 -->
           <input type="file" v-if="item.type === 'file'" accept="item.mime" />
         </label>
+        <!-- 单选 -->
+        <span v-if="item.type === 'radio'">
+          {{item.label.name}}
+          <label v-for='radio of item.radios.options' :key='radio.guid'>
+            <input type="radio" :value="radio.value" :name="item.radios.name" v-model="formData" />
+            {{radio.title}}
+          </label>
+        </span>
       </div>
     </form>
   </div>
 </template>
 
 <script>
+import guid from '@dllcn/js-shortid'
 export default {
   name: 'AdForm',
   data () {
     return {
+      formData: {
+
+      }
     }
+  },
+  created () {
+    this.forms.map(item => {
+      if (['radio'].indexOf(item.type) >= 0) {
+        switch(item.type) {
+          case 'radio':
+            item.radios.options.map(option => {
+              option.guid = guid.gen()
+            })
+            break;
+        }
+      }
+      item.guid = guid.gen()
+    })
   },
   props: {
     forms: Array
